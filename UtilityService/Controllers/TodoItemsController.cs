72c6e80d54b9c8eb25cb8c51e86e5210325b8b3d
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-02-27 23:19:46 +0800

    Add Utility Service

---
+++ UtilityService/Controllers/TodoItemsController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UtilityService.Data;
+using UtilityService.Models;
+
+namespace UtilityService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly UtilityDBContext _context;
+
+        public TodoItemsController(UtilityDBContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Get All Todo Items
+        /// </summary>
+        /// <returns> Todo Items </returns>
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/TodoItems/{id}
+        /// <summary>
+        /// Get Todo Item By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Todo Item By Id</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        /// <summary>
+        /// Put Todo Item By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="todoItem"></param>
+        /// <returns> Todo Item By ID</returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        /// <summary>
+        /// Post Todo Item By ID
+        /// </summary>
+        /// <param name="todoItem"></param>
+        /// <returns> Post Item By Id </returns>
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/{id}
+        /// <summary>
+        /// Delete item by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Delete Item By Id </returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return todoItem;
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ UtilityService/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿
+namespace UtilityService.Models
+{
+    public class TodoItem
+    {
+        /// <summary>
+        /// Todo ID
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// Todo Name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Todo IsComplete
+        /// </summary>
+        public bool IsComplete { get; set; }
+    }
+}
